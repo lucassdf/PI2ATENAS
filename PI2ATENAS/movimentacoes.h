4e2f8a6f5c7787dfa527dd653c6e3b9a22670899
lucassdf <lucas.souto.freitas@gmail.com>
2021-10-24 21:27:37 -0300

    Reorganizacao do codigo
    Foi feita uma reorganizacao do codigo, colocando a logica/funcoes dos tiros em arquivo separado

--- PI2ATENAS/movimentacoes.h
+++ PI2ATENAS/movimentacoes.h
@@ -56,3 +56,34 @@ void move_personagem(ALLEGRO_BITMAP* player, ALLEGRO_KEYBOARD_STATE keyState, in
 
 
 }
+
+//FUNCAO DE MOVIMENTO DO INIMIGO
+void move_inimigo(int* x, int* y, int* dir, int alt, int larg, ALLEGRO_BITMAP* inimigo)
+{
+
+	if (*dir == 1 && *x != 20 && *y != 20)
+	{
+		--* x;
+		--* y;
+	}
+	else if (*dir == 2 && *x != 20 && *y != alt - 50)
+	{
+		--* x;
+		++* y;
+	}
+	else if (*dir == 3 && *x != larg - 50 && *y != 20)
+	{
+		++* x;
+		--* y;
+	}
+	else if (*dir == 4 && *x != larg - 50 && *y != alt - 50)
+	{
+		++* x;
+		++* y;
+	}
+	else
+	{
+		*dir = rand() % 4 + 1;
+	}
+
+}
